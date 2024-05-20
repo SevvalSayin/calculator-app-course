aad0e371cc3cf7af593f5d79cd0bbf4532ef4d46
Sevval Sayın <ssayin0498@gmail.com>
2024-05-20 16:55:04 +0300

    useReducer yöntemi

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Calculator from './components/Calculator'
+import CalculatorUseReducer from './components/CalculatorUseReducer'
+
+function App() {
+
+  return (
+    <>
+     <Calculator/>
+     <CalculatorUseReducer/>
+    </>
+  )
+}
+
+export default App
